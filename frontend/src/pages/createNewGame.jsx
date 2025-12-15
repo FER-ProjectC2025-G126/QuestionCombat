823cefcd3d2ac25b08beb66a6e42f11d8cb31d54
Dorotea Perković <dorotea.perkovic@fer.hr>
2025-12-15 14:49:02 +0100

    Adding page for creating new game

--- frontend/src/pages/createNewGame.jsx
+++ frontend/src/pages/createNewGame.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuth } from "../features/auth/AuthProvider";
 import { useNavigate } from "react-router";
 import "../styles/createNewGame.css"
+import Button1 from "../components/Button1";
 
 
 function CreateNewGame() {
@@ -13,8 +14,50 @@ function CreateNewGame() {
 
     return <div>
         <div className="container">
-            <div className="block">
-                <p>New game</p>
+            <div className="block2">
+                <div className ="gameName">NEW GAME</div>
+                <div className ="block3">
+                    <div className="elementOfBlock2">
+                        <h2 className="elmHeader">CHOOSE NUMBER OF PLAYERS</h2>
+                            <form className="Forms">
+                               <label className="Labels">
+                                    <span>2 players</span>
+                                    <input type="radio" name="players" value="2" />
+                                </label>
+
+                                <label className="Labels">
+                                    <span>3 players</span>
+                                    <input type="radio" name="players" value="3" />
+                                </label>
+
+                                <label  className="Labels">
+                                    <span>4 players</span>
+                                    <input type="radio" name="players" value="4" />
+                                </label>
+                            </form>
+                    </div>
+                    <div className="elementOfBlock2">
+                        <h2 className="elmHeader">CHOOSE UP TO 5 COURSES</h2>
+                    </div>
+                    <div className="elementOfBlock2">
+                        <h2 className="elmHeader">PRIVATE OR PUBLIC?</h2>
+                        <form className="Forms">
+                               <label className="Labels">
+                                    <span>private</span>
+                                    <input type="radio" name="gameType" value="private" />
+                                </label>
+
+                                <label className="Labels">
+                                    <span>public</span>
+                                    <input type="radio" name="gametype" value="public" />
+                                </label>
+                        </form>
+                    </div>
+                    <div className="elementOfBlock2">
+                        <h2 className="elmHeader">READY?</h2>
+                        <Button1 text="START!" className="startBtn"/>
+                    </div>
+                </div>
             </div>
         </div>
         </div>;
