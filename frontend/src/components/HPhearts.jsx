f7c06463df98d472c5c6b30f5e16fd6588ad5b85
Antonio Mamić <antoniomamic007@gmail.com>
2026-01-29 00:24:58 +0100

    fix hearts not showing

--- frontend/src/components/HPhearts.jsx
+++ frontend/src/components/HPhearts.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import heart_full from "../assets/icons/hearts/heart-full.svg"
+import heart_half from "../assets/icons/hearts/heart-half.svg"
+import heart_empty from "../assets/icons/hearts/heart-empty.svg"
+
 
 const MAX_HEARTS = 5;
 const HP_PER_HEART = 2;
@@ -9,9 +13,9 @@ const HPHearts = ({ hp }) => {
       {Array.from({ length: MAX_HEARTS }).map((_, i) => {
         const heartHp = hp - i * HP_PER_HEART;
 
-        if (heartHp >= 2) return <img key={i} src="/heart-full.svg" alt="full heart" />;
-        if (heartHp === 1) return <img key={i} src="/heart-half.svg" alt="half heart" />;
-        return <img key={i} src="/heart-empty.svg" alt="empty heart" />;
+        if (heartHp >= 2) return <img key={i} src={heart_full} alt="full heart" />;
+        if (heartHp === 1) return <img key={i} src={heart_half} alt="half heart" />;
+        return <img key={i} src={heart_empty} alt="empty heart" />;
       })}
     </div>
   );
